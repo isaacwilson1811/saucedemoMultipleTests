afc93fdd43ecebdfc3ed0b5553a07c33715bc75a
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-03-25 12:59:51 -0600

    updated hamburger test to use cookie refactored login and logout method

--- test/specs/test.hamburger.js
+++ test/specs/test.hamburger.js
@@ -1,22 +1,28 @@
-import HomePage from '../pageobjects/homepage.js'
-import Authorized from '../pageobjects/authorized.js'
+import Authorize from '../pageobjects/authorize.js'
 import Hamburger from '../pageobjects/hamburger.js'
 
+// Should be able to run test with any valid user.
+// No password needed. Using cookies to log in.
 const userName = 'standard_user'
-const password = 'secret_sauce'
 
 // Login
-describe('Log in', () => {
+describe(`Log in as ${userName}`, () => {
     it('Should log in succesfully', async () => {
-        await HomePage.login(userName, password)
-        await Authorized.verifyLoggedIn()
+        await Authorize.login(userName)
     })
 })
 
 // Hamburger Menu Open
 describe('Open hamburger menu', () => {
     it('Should show expected menu items', async () => {
-        await Hamburger.buttonHamburger.click()
+        await Hamburger.buttonOpenMenu.click()
         await Hamburger.verifyMenuItems()
+    })
+})
+
+// Logout
+describe('Log out', () => {
+    it('Should log out succesfully', async () => {
+        await Authorize.logout()
     })
 })
